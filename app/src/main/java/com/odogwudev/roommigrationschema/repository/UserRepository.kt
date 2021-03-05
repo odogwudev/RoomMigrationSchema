9c215c9aee46ad48a504c89780a768c2f1a634ea
ghostadventures <47299656+ghostadventures@users.noreply.github.com>
2021-03-05 13:03:27 +0100

    Repository and ViewModel

---
+++ app/src/main/java/com/odogwudev/roommigrationschema/repository/UserRepository.kt
@@ -0,0 +1,71 @@
+package com.odogwudev.roommigrationschema.repository
+
+import androidx.annotation.WorkerThread
+import com.odogwudev.roommigrationschema.`interface`.UserDao
+import com.odogwudev.roommigrationschema.datamodel.UserEntity
+import com.odogwudev.roommigrationschema.datamodel.UserReportCardEntity
+import kotlinx.coroutines.flow.Flow
+
+class UserRepository(
+    private val userDao: UserDao
+) {
+
+    /***
+     * user_table methods
+     */
+
+    val allUserList: Flow<List<UserEntity>> = userDao.getAllUserList()
+
+    @Suppress("RedundantSuspendModifier")
+    @WorkerThread
+    suspend fun insertUser(userEntity: UserEntity) {
+        userDao.insertUser(userEntity)
+    }
+
+    @Suppress("RedundantSuspendModifier")
+    @WorkerThread
+    suspend fun deleteUser(userEntity: UserEntity) {
+        userDao.deleteUser(userEntity)
+    }
+
+    @Suppress("RedundantSuspendModifier")
+    @WorkerThread
+    suspend fun updateUser(userEntity: UserEntity) {
+        userDao.updateUser(userEntity)
+    }
+
+    @Suppress("RedundantSuspendModifier")
+    @WorkerThread
+    suspend fun deleteAllUserList() {
+        userDao.deleteAllUserList()
+    }
+
+    /***
+     * user_report_card_table methods
+     */
+    val allUserReportList: Flow<List<UserReportCardEntity>> = userDao.getAllUserReportList()
+
+    @Suppress("RedundantSuspendModifier")
+    @WorkerThread
+    suspend fun insertUserReport(userReportCardEntity: UserReportCardEntity) {
+        userDao.insertUserReport(userReportCardEntity)
+    }
+
+    @Suppress("RedundantSuspendModifier")
+    @WorkerThread
+    suspend fun deleteUserReport(userReportCardEntity: UserReportCardEntity) {
+        userDao.deleteUserReport(userReportCardEntity)
+    }
+
+    @Suppress("RedundantSuspendModifier")
+    @WorkerThread
+    suspend fun updateUserReport(userReportCardEntity: UserReportCardEntity) {
+        userDao.updateUserReport(userReportCardEntity)
+    }
+
+    @Suppress("RedundantSuspendModifier")
+    @WorkerThread
+    suspend fun deleteAllUserReportList() {
+        userDao.deleteAllUserReportList()
+    }
+}
---
+++ app/src/main/java/com/odogwudev/viewmodel/UserReportViewModel.kt
@@ -0,0 +1,38 @@
+package com.odogwudev.viewmodel
+
+import androidx.lifecycle.*
+import com.odogwudev.roommigrationschema.datamodel.UserReportCardEntity
+import com.odogwudev.roommigrationschema.repository.UserRepository
+import kotlinx.coroutines.launch
+
+class UserReportViewModel(private val repository: UserRepository) : ViewModel() {
+
+    val allUserReportList: LiveData<List<UserReportCardEntity>> =
+        repository.allUserReportList.asLiveData()
+
+    fun insertUserReport(userReportCardEntity: UserReportCardEntity) = viewModelScope.launch {
+        repository.insertUserReport(userReportCardEntity)
+    }
+
+    fun deleteUserReport(userReportCardEntity: UserReportCardEntity) = viewModelScope.launch {
+        repository.deleteUserReport(userReportCardEntity)
+    }
+
+    fun updateUserReport(userReportCardEntity: UserReportCardEntity) = viewModelScope.launch {
+        repository.updateUserReport(userReportCardEntity)
+    }
+
+    fun deleteAllUserReport() = viewModelScope.launch {
+        repository.deleteAllUserReportList()
+    }
+
+    class userViewModelFactory(private val repository: UserRepository) : ViewModelProvider.Factory {
+        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(UserReportViewModel::class.java)) {
+                @Suppress("UNCHECKED_CAST")
+                return UserReportViewModel(repository) as T
+            }
+            throw IllegalStateException("Unknown ViewMOdel Class")
+        }
+    }
+}
